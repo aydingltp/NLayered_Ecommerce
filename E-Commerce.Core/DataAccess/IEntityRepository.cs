e875dd8f3b1f35c250c48009d508f235d0f85aeb
aydın gültepe <aydngltp@gmail.com>
2019-03-12 11:29:45 +0300

    IEntityRepository CRUD Metots Added

--- E-Commerce.Core/DataAccess/IEntityRepository.cs
+++ E-Commerce.Core/DataAccess/IEntityRepository.cs
@@ -6,8 +6,14 @@ using E_Commerce.Core.Entities;
 
 namespace E_Commerce.Core.DataAccess
 {
-    public interface IEntityRepository
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
-        
+        T Get(Expression<Func<T, bool>> filter = null);
+        List<T> GetList(Expression<Func<T,bool>> filter=null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+         
+
     }
 }
