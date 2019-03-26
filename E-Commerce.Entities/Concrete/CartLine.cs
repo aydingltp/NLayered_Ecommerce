a511f29f4737ab967d1c1d65a6d91a68ab97da11
aydın gültepe <aydngltp@gmail.com>
2019-03-26 19:08:48 +0300

    cart ve cartline sinif

---
+++ E-Commerce.Entities/Concrete/CartLine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+
+namespace E_Commerce.Entities.Concrete
+{
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
