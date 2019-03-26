6d4aba5f66f18e04234e6020f5d62f34d76218b0
aydın gültepe <aydngltp@gmail.com>
2019-03-26 19:32:50 +0300

    cart service

---
+++ E-Commerce.Business/Abstract/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using E_Commerce.Entities.Concrete;
+
+namespace E_Commerce.Business.Abstract
+{
+    public interface ICartService
+    {
+        void AddCart(Cart cart,Product product);
+        void RemoveFromCart(Cart cart,int producId);
+        List<CartLine> List(Cart cart);
+    }
+}
---
+++ E-Commerce.Business/Concrete/CartService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using E_Commerce.Business.Abstract;
+using E_Commerce.Entities.Concrete;
+using Microsoft.EntityFrameworkCore.Query.ExpressionTranslators.Internal;
+
+namespace E_Commerce.Business.Concrete
+{
+    public class CartService:ICartService
+    {
+        public void AddCart(Cart cart, Product product)
+        {
+            CartLine cartLine = cart.CartLines.FirstOrDefault(i => i.Product.ProductId == product.ProductId);
+            if (cartLine!=null)
+            {
+                cartLine.Quantity++;
+                return;
+            }
+            else
+            {
+                cart.CartLines.Add(new CartLine{Product = product, Quantity = 1});
+            }
+        }
+
+        public void RemoveFromCart(Cart cart, int producId)
+        {
+            cart.CartLines.Remove(cart.CartLines.FirstOrDefault(i => i.Product.ProductId == producId));
+        }
+
+        public List<CartLine> List(Cart cart)
+        {
+            return cart.CartLines;
+        }
+    }
+}
