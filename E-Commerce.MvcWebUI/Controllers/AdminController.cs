8b64417b45c510b668398ed21006417248017e8a
aydın gültepe <aydngltp@gmail.com>
2019-03-28 13:31:43 +0300

    admin product update

--- E-Commerce.MvcWebUI/Controllers/AdminController.cs
+++ E-Commerce.MvcWebUI/Controllers/AdminController.cs
@@ -50,5 +50,29 @@ namespace E_Commerce.MvcWebUI.Controllers
 
             return RedirectToAction("Add");
         }
+
+        public ActionResult Update(int productId)
+        {
+            var model = new ProductUpdateViewModel
+            {
+                Product=_productService.GetById(productId),
+                Categories=_categoryService.GetAll()
+
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Update(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productService.Update(product);
+                TempData.Add("message", "Product was successfully updated");
+
+            }
+
+            return RedirectToAction("Update");
+        }
+
     }
 }
