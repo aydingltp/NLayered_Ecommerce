230afc4e3ab76e6cc632e9503578106de43bc4dd
aydın gültepe <aydngltp@gmail.com>
2019-04-16 22:19:19 +0300

    Account controller

---
+++ E-Commerce.MvcWebUI/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using E_Commerce.MvcWebUI.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.MvcWebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<CustomIdentityUser> _userManager;
+        private RoleManager<CustomIdentityRole> _roleManager;
+        private SignInManager<CustomIdentityUser> _signInManager;
+
+        public AccountController(SignInManager<CustomIdentityUser> signInManager, RoleManager<CustomIdentityRole> roleManager, UserManager<CustomIdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        
+    }
+}
